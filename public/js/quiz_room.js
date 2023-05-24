9970fd72dbfb00b24b94e7f3ba67668cab4cc673
BuildTools <unconfigured@null.spigotmc.org>
2023-05-24 18:10:05 +0200

    :boom:

--- public/js/quiz_room.js
+++ public/js/quiz_room.js
@@ -1,23 +1,47 @@
+// Initialiseer de socket verbinding.
 const socket = io();
 
+// Verkrijg de elementen van de DOM.
 const countdownElement = document.getElementById('countdown');
 const previewElement = document.getElementById('preview');
 const optionsElement = document.getElementById('options');
 const scoreboardElement = document.getElementById('scoreboard');
 
+// Standaard is de huidige speler geen host.
 let isHost = false;
 
+// Event listener voor de update van de algemene countdown.
 socket.on('updateCountdown', (countdown) => {
+  // Verberg de countdown als deze minder dan 0 is.
   if (countdown < 0) {
     countdownElement.style.display = 'none';
   } else if (countdown === 10) {
+    // Geef een bericht weer als de countdown gelijk is aan 10.
     countdownElement.innerText = "Het spel wordt automatisch gestart als er 3 spelers in deze room zitten";
   } else {
+    // Toon de countdown
     countdownElement.style.display = 'block';
     countdownElement.innerText = `We beginnen in .. ${countdown}`;
   }
 });
 
+// Event listener voor de update van de ronde countdown.
+socket.on('updateRoundCountdown', ({ countdown, roundNumber }) => {
+  const countdownElement = document.getElementById('roundCountdown');
+  // Verberg de countdown als deze gelijk of minder is dan 0.
+  if (countdown <= 0) {
+    countdownElement.style.display = 'none';
+  } else {
+    // Toon de countdown voor de volgende ronde.
+    countdownElement.style.display = 'block';
+    countdownElement.innerText = `Ronde [${roundNumber}] gaat beginnen in ${countdown}...`;
+
+    // Pauzeer het voorbeeld van de track.
+    previewElement.pause();
+  }
+});
+
+// Event listener om de antwoordknoppen te activeren.
 socket.on('enableButtons', () => {
   const buttons = optionsElement.getElementsByTagName('button');
   for (let i = 0; i < buttons.length; i++) {
@@ -25,6 +49,7 @@ socket.on('enableButtons', () => {
   }
 });
 
+// Event listener om de antwoordknoppen te deactiveren.
 socket.on('disableButtons', () => {
   const buttons = optionsElement.getElementsByTagName('button');
   for (let i = 0; i < buttons.length; i++) {
@@ -32,8 +57,12 @@ socket.on('disableButtons', () => {
   }
 });
 
+// Event listener voor het ontvangen van een nieuwe vraag.
 socket.on('newQuestion', (question) => {
+  // Controleer of de huidige speler de host is.
   isHost = socket.id === question.hostId;
+
+  // Stel het voorbeeld van de track en de antwoordopties in.
   previewElement.src = question.previewUrl;
   optionsElement.innerHTML = '';
   question.options.forEach((artist, index) => {
@@ -46,32 +75,49 @@ socket.on('newQuestion', (question) => {
   });
 });
 
+// Event listener voor het afspelen van het track voorbeeld zodra het geladen is.
 previewElement.addEventListener('canplaythrough', () => {
   previewElement.play();
 });
 
+// Event listener voor het updaten van het scoreboard.
+// Event listener voor het updaten van het scoreboard.
 socket.on('updateScoreboard', (players) => {
   scoreboardElement.innerHTML = '';
   for (const playerId in players) {
     const player = players[playerId];
     const scoreEntry = document.createElement('div');
-    scoreEntry.innerText = `${player.username}: ${player.score}`;
+
+    // Als de huidige gebruiker de speler is, voeg dan "(Jij)" toe achter de naam
+    if (player.username === username) {
+      scoreEntry.innerText = `${player.username} (Jij): ${player.score}`;
+    } else {
+      scoreEntry.innerText = `${player.username}: ${player.score}`;
+    }
+
     scoreboardElement.appendChild(scoreEntry);
   }
 });
 
+
+// Event listener voor het einde van het track voorbeeld.
 previewElement.addEventListener('ended', () => {
+  // Als de huidige speler de host is, ga dan naar de volgende ronde.
   if (isHost) {
     socket.emit('nextRound', roomName);
   }
 });
 
+// Event listener voor het doorsturen naar de resultatenpagina.
 socket.on('redirectToResults', (winner) => {
   window.location.href = `/quiz_result?winner=${encodeURIComponent(winner)}`;
 });
 
+// Haal de gebruikersnaam en de naam van de kamer op uit de URL parameters.
 const username = new URLSearchParams(window.location.search).get('username');
 const roomName = new URLSearchParams(window.location.search).get('room');
+
+// Als beide aanwezig zijn, stuur dan een verzoek om de kamer te betreden.
 if (username && roomName) {
   socket.emit('join', { username, room: roomName });
 }
--- socket.js
+++ socket.js
@@ -1,8 +1,8 @@
 module.exports = function initializeSocket(io) {
-  // Importeer de functies die nodig zijn om de data te verkrijgen van de Spotify API
+
   const { getRandomTrack, getRandomArtists } = require('./api/spotify');
 
-  // Functie om een willekeurige Room ID te genereren
+  // Functie om willekeurige RoomID te genereren.
   function generateRandomRoomId() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const length = 6;
@@ -15,10 +15,10 @@ module.exports = function initializeSocket(io) {
     return result;
   }
 
-  // Object dat alle actieve rooms bijhoudt
+  // Object dat alle rooms bijhoudt.
   const rooms = {};
 
-  // Functie om de status van een bepaalde room op te vragen of te initialiseren als deze nog niet bestaat
+  // Functie om de status van een bepaalde room op te vragen of te initialiseren mocht deze nog niet bestaan.
   function getRoomState(room) {
     if (!rooms[room]) {
       rooms[room] = {
@@ -28,76 +28,80 @@ module.exports = function initializeSocket(io) {
         playersAnswered: 0,
         tracks: [],
         host: null,
-        tracksLoaded: false, // nieuwe eigenschap
+        tracksLoaded: false,
+        roundNumber: 0,
       };
     }
     return rooms[room];
   }
 
-  // Start luisteren naar verbindingen
+  // Start luisteren naar verbindingen.
   io.on('connection', (socket) => {
-    // Als een speler een room wil joinen
+
+    // Als een speler een room wil joinen.
     socket.on('join', ({ username, room }) => {
       const gameState = getRoomState(room);
       socket.join(room);
 
-      // Voeg de speler toe aan de room
+      // Voeg de speler toe aan de room.
       gameState.players[socket.id] = { username, score: 0, hasAnswered: false };
-      // Update het scoreboard
+
+      // Updaten van het scoreboard.
       io.to(room).emit('updateScoreboard', gameState.players);
 
-      // Controleer aantal spelers in de room en start het spel indien nodig
+      // Controleer aantal spelers in de room en start het spel indien nodig.
       const playerCount = Object.keys(gameState.players).length;
       if (playerCount === 10) {
         startGame(room);
       } else if (playerCount >= 3 && gameState.countdown === 10) {
-        // Begin met het aftellen, ongeacht of de tracks al zijn opgehaald
+
+        // Begin met het aftellen, ongeacht of de tracks al zijn opgehaald.
         const countdownInterval = setInterval(() => {
           gameState.countdown -= 1;
           io.to(room).emit('updateCountdown', gameState.countdown);
 
           if (gameState.countdown === 0) {
             clearInterval(countdownInterval);
-            if (gameState.tracksLoaded) { // controleer of de tracks zijn geladen
+            if (gameState.tracksLoaded) { // controleer of de tracks zijn geladen.
               startGame(room);
             } else {
-              // Hier kun je de aftelling herstarten of het spel pauzeren tot de tracks zijn geladen
+              console.log("Tracks zijn nog niet geladen.")
             }
           }
         }, 1000);
 
-        // Begin met het ophalen van de tracks terwijl het aftellen aan de gang is
+        // Begin met het ophalen van de tracks terwijl het aftellen aan de gang is.
         if (!gameState.tracksLoaded) {
           populateTracks(gameState);
         }
       }
     });
 
-    // Als een speler een nieuwe room wil maken
+    // Als een speler een nieuwe room wil maken.
     socket.on('createRoom', () => {
       const room = generateRandomRoomId();
       getRoomState(room);
       socket.emit('roomCreated', room);
     });
 
-    // Als een speler een antwoord instuurt
+    // Als een speler een antwoord instuurt.
     socket.on('submitAnswer', ({ answer, room }) => {
       const gameState = getRoomState(room);
       const player = gameState.players[socket.id];
 
-      // Controleer of de speler nog niet heeft geantwoord en er een actieve vraag is
+      // Controleer of de speler nog niet heeft geantwoord en er een actieve vraag is.
       if (!player.hasAnswered && gameState.currentQuestion) {
         player.hasAnswered = true;
         gameState.playersAnswered++;
 
-        // Check of het antwoord correct was
+        // Check of het antwoord correct was.
         let correctAnswerGiven = answer === gameState.currentQuestion.correctAnswer;
 
         if (correctAnswerGiven) {
           player.score += 1;
           io.to(room).emit('updateScoreboard', gameState.players);
 
-          // Als een speler 10 punten heeft behaald
+          // Zodra een speler 10 punten heeft behaald.
           if (player.score === 10) {
             io.to(room).emit('redirectToResults', player.username);
           } else {
@@ -105,7 +109,7 @@ module.exports = function initializeSocket(io) {
           }
         }
 
-        // Als alle spelers hebben geantwoord, ga dan naar de volgende vraag
+        // Als alle spelers hebben geantwoord, forceren we de volgende vraag.
         if (gameState.playersAnswered === Object.keys(gameState.players).length) {
           if (!correctAnswerGiven) {
             nextQuestion(room);
@@ -114,12 +118,10 @@ module.exports = function initializeSocket(io) {
           }
         }
 
-        // Deactiveer de antwoordknoppen voor de huidige vraag
         socket.emit('disableButtons');
       }
     });
 
-    // Als de host besluit om naar de volgende ronde te gaan
     socket.on('nextRound', (room) => {
       const gameState = getRoomState(room);
       if (socket.id === gameState.host) {
@@ -127,11 +129,9 @@ module.exports = function initializeSocket(io) {
       }
     });
 
-    // Wanneer een speler de verbinding verbreekt
+    // Wanneer een speler de verbinding verbreekt.
     socket.on('disconnect', () => {
-      // console.log('Disconnected:', socket.id);
       const room = Object.keys(socket.rooms).find((r) => r !== socket.id);
-      // console.log('Room:', room);
       if (room) {
         const gameState = getRoomState(room);
         delete gameState.players[socket.id];
@@ -140,7 +140,7 @@ module.exports = function initializeSocket(io) {
     });
   });
 
-  // Functie om tracks uit Spotify API te halen en toe te voegen aan de gameState
+  // Functie om tracks uit Spotify API te halen en toe te voegen aan de gameState.
   async function populateTracks(gameState) {
     const playlistId = "37i9dQZEVXbNG2KDcFcKOF";
     for (let i = 0; i < 15; i++) {
@@ -150,7 +150,7 @@ module.exports = function initializeSocket(io) {
     gameState.tracksLoaded = true;
   }
 
-  // Functie om het spel te starten
+  // Functie om het spel te starten.
   async function startGame(room) {
     const gameState = getRoomState(room);
     gameState.countdown = -1;
@@ -159,17 +159,19 @@ module.exports = function initializeSocket(io) {
     nextQuestion(room);
   }
 
-  // Functie om een host te selecteren voor de game
+  // Functie om een host te selecteren voor de game.
   function selectHost(gameState) {
     const playerIds = Object.keys(gameState.players);
     return playerIds[Math.floor(Math.random() * playerIds.length)];
   }
 
 
-  // Functie om naar de volgende vraag te gaan
+  // Functie om naar de volgende vraag te gaan.
   async function nextQuestion(room) {
     const gameState = getRoomState(room);
     gameState.playersAnswered = 0;
+
+    gameState.roundNumber++;
 
     if (gameState.tracks.length === 0) {
       await populateTracks(gameState);
@@ -184,12 +186,27 @@ module.exports = function initializeSocket(io) {
       options: artists.sort(() => Math.random() - 0.5),
       correctAnswer: track.artist,
     };
-    io.to(room).emit('newQuestion', gameState.currentQuestion);
 
-    // Reset de hasAnswered status van de spelers en activeer de knoppen
+    // Start de countdown voor iedere ronde.
+    let roundCountdown = 5;
+
     for (const playerId in gameState.players) {
-      gameState.players[playerId].hasAnswered = false;
-      io.to(room).to(playerId).emit('enableButtons');
+      io.to(room).to(playerId).emit('disableButtons');
     }
+
+    const roundCountdownInterval = setInterval(() => {
+      roundCountdown -= 1;
+      io.to(room).emit('updateRoundCountdown', { countdown: roundCountdown, roundNumber: gameState.roundNumber });
+      if (roundCountdown === 0) {
+        clearInterval(roundCountdownInterval);
+        io.to(room).emit('newQuestion', gameState.currentQuestion);
+
+        // Reset de hasAnswered status van de spelers en activeer de knoppen.
+        for (const playerId in gameState.players) {
+          gameState.players[playerId].hasAnswered = false;
+          io.to(room).to(playerId).emit('enableButtons');
+        }
+      }
+    }, 1000);
   }
 };
